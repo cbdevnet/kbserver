c61ace22c91d3ab30b18a147ef48a0ff8239eee0
cbdev <cb@cbcdn.com>
2014-05-02 11:52:23 +0200

    beginning config file parser

--- kbactiond/cfgparse.c
+++ kbactiond/cfgparse.c
@@ -1,3 +1,24 @@
 bool cfg_read(ARGUMENTS* args, CONFIG* cfg){
+	FILE* cfg;
+
+	if(!args->cfgfile){
+		if(args->verbosity>0){
+			printf("No config file supplied, aborting\n");
+		}
+		return false;
+	}
+
+	cfg=fopen(args->cfgfile, "r");
+	if(!cfg){
+		if(args->verbosity>0){
+			perror("read_config");
+		}
+		return false;
+	}
+
+	//TODO read config file
+
+	fclose(cfg);
+
 	return false;
 }
